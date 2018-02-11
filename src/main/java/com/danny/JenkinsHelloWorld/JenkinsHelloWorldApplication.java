9bc35aaa37e2e2bed007cf18ca1e5b6e7ce5075a
Danny King <kingd@oclc.org>
2018-02-11 11:35:23 -0500

    initial commit

---
+++ src/main/java/com/danny/JenkinsHelloWorld/JenkinsHelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.danny.JenkinsHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsHelloWorldApplication.class, args);
+	}
+}
